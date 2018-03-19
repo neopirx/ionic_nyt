188001039ec835a4fee2515589cca14a2c990581
neopirx <emulgator@protonmail.ch>
2018-03-19 19:42:26 +0100

    added date to posts, fixed problems with displaying titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
-
+  
   pages: Array<{title: string, component: any}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,8 +9,6 @@ import { NewsProvider } from '../../providers/news/news';
 
 export class HomePage {
   news: any;
-  selectedNews: any;
-  newsLength: any;
 
   constructor(public navCtrl: NavController, private newsProvider: NewsProvider) { 
 
@@ -19,18 +17,7 @@ export class HomePage {
   ionViewWillEnter() { 
     this.newsProvider.getNewsHome().subscribe(news => {
       this.news = news;
-      this.newsLength = this.news.length;
       console.log(news);
-      select();
   });
-  }
-}
-
-
-function select() {
-  var counter: 0;
-  for(var i = 1; i < this.newsLength; i++) {
-    this.selectedNews[counter] = 'dick';
-    counter++;
   }
 }
